a817863d0d4eb6f61161bb70e7d09d8682b64619
Javier <javninpen@alu.edu.gva.es>
2025-12-17 17:37:28 +0100

    Actualiza el favicon y el título de la aplicación a KickMatch; elimina importaciones innecesarias en DashboardPage.

--- frontend/src/pages/DashboardPage.js
+++ frontend/src/pages/DashboardPage.js
@@ -1,13 +1,6 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { useContext } from 'react'
-import { AuthContext } from '../context/authContext'
 import { Nav } from '../components/nav/Nav'
 
 export const DashboardPage = () => {
-
-    const {logout} = useContext(AuthContext)
-
   return (
     <main>
       <Nav/>
