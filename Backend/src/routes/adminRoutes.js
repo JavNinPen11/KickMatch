d221ebe80aaf1b49d7818142294af3e8bb2955a6
JAVIER SANTIAGO NIÑO PEÑALOZA <javninpen@alu.edu.gva.es>
2026-05-24 09:49:51 +0200

    feat: añadir rutas para eliminar y cancelar partidos con verificación de permisos

--- Backend/src/routes/adminRoutes.js
+++ Backend/src/routes/adminRoutes.js
@@ -2,6 +2,7 @@ import express from "express"
 import { getAllMatches } from "../controllers/adminController.js"
 import { verifyToken } from "../middleware/authMiddleware.js"
 import { isAdmin } from "../middleware/adminMiddleware.js"
+import { cancelMatch, deleteMatch } from "../controllers/matchController.js"
 
 const router = express.Router()
 
@@ -9,5 +10,7 @@ router.get("/getMatches", verifyToken, isAdmin, getAllMatches)
 router.get("/role", verifyToken, (req, res) => {
     res.json({ role: req.user.role })
 })
+router.delete("/matches/:id", verifyToken, isAdmin, deleteMatch)
+router.put("/matches/:id/cancel", verifyToken, isAdmin, cancelMatch)
 
 export default router
