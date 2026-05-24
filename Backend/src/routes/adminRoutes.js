ee62243465c05b0bcfdb7307499792aa4531ffb0
JAVIER SANTIAGO NIÑO PEÑALOZA <javninpen@alu.edu.gva.es>
2026-05-24 09:59:13 +0200

    feat: añadir importación de adminDeleteUser en las rutas de administrador

--- Backend/src/routes/adminRoutes.js
+++ Backend/src/routes/adminRoutes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { adminUpdateUser, getAllMatches } from "../controllers/adminController.js"
+import { adminDeleteUser, adminUpdateUser, getAllMatches } from "../controllers/adminController.js"
 import { verifyToken } from "../middleware/authMiddleware.js"
 import { isAdmin } from "../middleware/adminMiddleware.js"
 import { cancelMatch, deleteMatch } from "../controllers/matchController.js"
