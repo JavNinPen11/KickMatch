09d8450f31ee1989d41714612283452dec61d42d
JAVIER SANTIAGO NIÑO PEÑALOZA <javninpen@alu.edu.gva.es>
2026-05-24 08:26:52 +0200

    fix: añadir verificación de token en la ruta de creación de partidos

--- Backend/src/routes/matchRoutes.js
+++ Backend/src/routes/matchRoutes.js
@@ -4,7 +4,7 @@ import { verifyToken } from "../middleware/authMiddleware.js"
 
 const router = express.Router()
 
-router.post("/createMatch", createMatch)
+router.post("/createMatch", verifyToken, createMatch)
 router.get("/myMatches", verifyToken, myMatches)
 router.get("/getMatches", getMatches)
 router.delete("/deleteMatch", deleteMatch)
